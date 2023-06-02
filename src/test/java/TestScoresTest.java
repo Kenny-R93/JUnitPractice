8ceb5d3ebc511f67a8b403808a0e2a25fb4c50dd
Kenny-R93 <krangelunc@gmail.com>
2023-06-02 00:35:48 -0400

    I am seeking, I am striving, I am in it with all my heart.

--- src/test/java/TestScoresTest.java
+++ src/test/java/TestScoresTest.java
@@ -1,11 +1,36 @@
 import org.example.TestScores;
+import org.junit.After;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TestScoresTest {
     @Test
-    void NinetyOrHigherReturnsA(){
+    void fiftyNineOrLowerReturnsF() {
+        TestScores F = new TestScores();
+        assertEquals("F", F.calculateGrade(59));
+    }
+
+    @Test
+    void sixtyReturnsD() {
+        TestScores D = new TestScores();
+        assertEquals("D", D.calculateGrade(60));
+    }
+
+    @Test
+    void seventyReturnsC() {
+        TestScores C = new TestScores();
+        assertEquals("C", C.calculateGrade(70));
+    }
+
+    @Test
+    void eightyReturnsB() {
+        TestScores B = new TestScores();
+        assertEquals("B", B.calculateGrade(80));
+    }
+
+    @Test
+    void ninetyOrHigherReturnsA() {
         TestScores A = new TestScores();
         assertEquals("A", A.calculateGrade(90));
     }
